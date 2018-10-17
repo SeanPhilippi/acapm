5dfbd28584ae56453d41718579ad444d684b263e
SeanPhilippi <sean.philippi@gmail.com>
2018-10-16 22:05:39 -0500

    finished

--- create-package-json.js
+++ create-package-json.js
@@ -31,31 +31,48 @@ if (firstArg === 'init') {
       console.log('package.json already exists.  Exiting...');
       process.exit();
    } else {
-      console.log('This utility will walk you through creating a package.json file. \n Press ^C at any time to quit.');
+      console.log('This utility will walk you through creating a package.json file. \nPress ^C at any time to quit.\n');
 
       rl.question('name: ', name => {
          rl.question('version: ', vers => {
             rl.question('description: ', desc => {
                rl.question('entry point: ', entPnt => {
-                  rl.question('test command: ', testCmd => {
+                  rl.question('keywords: ', keywords => {
                      rl.question('git repository: ', gitRepo => {
-                        // package.json object with user inputs as key values
-                        const packageJson = {
-                           name: name,
-                           version: vers,
-                           description: desc,
-                           'entry point': entPnt,
-                           'test command': testCmd,
-                           'git repository': gitRepo
-                        };
-
-                        fs.writeFile('package.json', packageJson, (err) => {
-                           // throws error if error
-                           if (err) throw err;
-                           // success case, file is saved
-                           console.log('package.json successfully created!')
+                        rl.question('author: ', author => {
+                           // package.json object with user inputs as key values
+                           const jsonObj = {
+                              name: name,
+                              version: vers,
+                              description: desc,
+                              main: entPnt,
+                              keywords: keywords,
+                              repository: gitRepo,
+                              author: author
+                           };
+   
+                           // storing current path in variable
+                           const currentPath = process.cwd();
+                           console.log('\nAbout to write to ' + currentPath + '/package.json:');
+                           // display object that will be written to package.json
+                           console.log('\n');
+                           console.log(jsonObj);
+                           console.log('\n');
+   
+                           rl.question('Is this ok? (yes) ', answer => {
+                              if (answer === 'yes' || answer === 'Yes' || answer === 'y' || answer === 'Y') {
+                                 // creates package.json and writes jsonObj to
+                                 fs.writeFile('package.json', jsonObj, (err) => {
+                                       // throws error if error
+                                       if (err) throw err;
+                                       // success case, file is saved
+                                       console.log('package.json successfully created!')
+                                 })
+                              } else {
+                                 process.exit();
+                              }
+                           })
                         })
-
                      })
                   })
                })
@@ -63,6 +80,9 @@ if (firstArg === 'init') {
          })
       })
    }
+} else {
+   console.log("Error: use 'init' as first argument");
+   process.exit();
 }
 
 
